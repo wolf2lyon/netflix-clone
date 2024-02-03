60f96f449653d62b9d322838436c40b0fb2e6d2c
wolf2lyon <josesebastiancu@gmail.com>
2024-02-03 11:21:20 -0500

    create auth

---
+++ app/auth/page.tsx
@@ -0,0 +1,13 @@
+const Auth = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')]">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+            <img src="/images/Logo.png" alt="logo" className="h-12" />
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
